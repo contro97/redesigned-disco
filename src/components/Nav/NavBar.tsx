c344e286d1e9cd73fab7225b3a7edfd72d1455c0
Ciaran Mahon <ciaranmahon97@gmail.com>
2023-09-04 15:53:48 -0700

    init commit

---
+++ src/components/Nav/NavBar.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import NavButton from "./NavButton";
+
+export function NavBar(){
+    return(
+    <div>Navbar works!
+        <NavButton/>
+    </div>
+    )
+}
+
+export default NavBar;
---
+++ src/components/Nav/NavButton.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+export function NavButton(){
+    return(
+    <div>
+        <button>Home</button>
+        <button>Projects</button>
+    </div>
+    )
+}
+
+export default NavButton;
